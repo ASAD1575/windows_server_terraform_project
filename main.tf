839944ae446ce22de7399f890563c4824131c22f
ASAD1575 <masadsubhani8@gmail.com>
2025-09-12 00:36:58 +0500

    vpc and sg module called in root main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ provider "aws" {
   region = var.aws_region
   
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+  azs = var.azs
+}
+
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
+  
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" { 
   type = string
 }
+variable "vpc_cidr_block" { type = string }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
+variable "azs" { type = list(string) }
